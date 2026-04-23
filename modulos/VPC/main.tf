8396e392dcb6a0d291f943862b11f0fb24b2289e
alexistonato999-hub <alexistonato999@gmail.com>
2026-04-22 20:33:31 -0500

    Diseño de subredes publicas y privadas

--- modulos/VPC/main.tf
+++ modulos/VPC/main.tf
@@ -18,3 +18,43 @@ resource "aws_vpc" "main" {
     Entorno     = "Produccion"
   }
 }
+
+# Fase 2: Diseño de Subredes Publicas y Privadas
+resource "aws_subnet" "public_web" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true  
+
+  tags = {
+    Name = "Subnet-Public-${count.index + 1}-${var.Region_name}"
+  }
+}
+
+resource "aws_subnet" "private_app" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, count.index + 10)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "Subnet-App-${count.index + 1}-${var.Region_name}"
+  }
+}
+
+resource "aws_subnet" "private_db" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, count.index + 20)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "Subnet-DB-${count.index + 1}-${var.Region_name}"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
