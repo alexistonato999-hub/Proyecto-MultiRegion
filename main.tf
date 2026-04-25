e4d5f808077237b56112d4d7382679b1984103d1
alexistonato999-hub <alexistonato999@gmail.com>
2026-04-25 12:15:41 -0500

    Configuracion Interconexion entre VPCS

--- main.tf
+++ main.tf
@@ -11,3 +11,26 @@ module "VPC_Region_B" {
   cidr_block= "10.20.0.0/16"
   Region_name = "Region-Pasiva"
 }
+
+# Fase 7: Conexion entre VPCs
+resource "aws_vpc_peering_connection" "virginia_to_london" {
+  provider      = aws.Region-Activa-A
+  peer_vpc_id   = module.VPC_Region_B.vpc_id
+  vpc_id        = module.VPC_Region_A.vpc_id
+  peer_region   = "eu-west-2"
+  auto_accept   = false
+
+  tags = {
+    Name = "Peering-Virginia-Londres"
+  }
+}
+
+resource "aws_vpc_peering_connection_accepter" "london_accept" {
+  provider                  = aws.Region-Pasiva-B
+  vpc_peering_connection_id = aws_vpc_peering_connection.virginia_to_london.id
+  auto_accept               = true
+
+  tags = {
+    Name = "Peering-Aceptado-Londres"
+  }
+}
---
+++ modulos/VPC/outputs.tf
@@ -0,0 +1,32 @@
+output "vpc_id" {
+  value       = aws_vpc.main.id
+  description = "ID de la VPC para asociar el Peering y otros recursos regionales"
+}
+output "public_subnet_ids" {
+  value       = aws_subnet.public_web[*].id
+  description = "IDs de subredes públicas donde reside el ALB"
+}
+output "private_app_subnet_ids" {
+  value       = aws_subnet.private_app[*].id
+  description = "IDs de subredes privadas para las instancias EC2 (Capa de Aplicación)"
+}
+output "security_group_app_id" {
+  value       = aws_security_group.sg_app.id
+  description = "SG de la App para que el módulo de Cómputo pueda recibir tráfico del ALB"
+}
+output "security_group_db_id" {
+  value       = aws_security_group.sg_db.id
+  description = "SG de la DB para permitir conexiones desde la capa de aplicación"
+}
+output "alb_dns_name" {
+  value       = aws_lb.alb.dns_name
+  description = "Nombre DNS del balanceador; entrada vital para CloudFront y Route 53"
+}
+output "alb_zone_id" {
+  value       = aws_lb.alb.zone_id
+  description = "ID de zona del ALB, necesario para crear registros tipo ALIAS en Route 53"
+}
+output "target_group_arn" {
+  value       = aws_lb_target_group.app_tg.arn
+  description = "ARN del Target Group para que el Auto Scaling Group sepa dónde registrar las instancias"
+}
