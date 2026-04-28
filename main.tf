18a95465c9004a29dd4f45eee371ccca5fa71028
alexistonato999-hub <alexistonato999@gmail.com>
2026-04-27 21:09:55 -0500

    Configuracion Amazon CloudFront

--- main.tf
+++ main.tf
@@ -92,3 +92,66 @@ resource "aws_route53_record" "secondary" {
 
   set_identifier = "London-Passive"
 }
+
+# Fase 9: Distribución de CloudFront y asociacion a ALBs
+resource "aws_cloudfront_distribution" "main_distribution" {
+  provider = aws.Region-Activa-A
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Distribución Global para Sistema de Respaldo Multi-Región"
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = module.VPC_Region_A.alb_dns_name
+    origin_id   = "ALB-Virginia"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only" # añadir el certificado SSL
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  origin {
+    domain_name = module.VPC_Region_B.alb_dns_name
+    origin_id   = "ALB-London"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "ALB-Virginia" 
+
+    forwarded_values {
+      query_string = true
+      cookies { forward = "all" }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Entorno = "Produccion"
+  }
+}
